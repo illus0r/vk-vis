736ea113f89dc8d661ad3fd96f272923bc11afe8
illus0r <zor667@gmail.com>
2019-04-02 19:47:21 +0400

    init

---
+++ script.js
@@ -0,0 +1,13 @@
+
+function setup() {
+  var canvas = createCanvas(100, 100);
+  canvas.parent('canvas-wrapper');
+  background(255, 0, 200);
+	ellipse(50, 50, 80, 80);
+}
+
+function draw() {
+}
+
+
+
