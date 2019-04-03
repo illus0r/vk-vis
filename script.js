14c169f894fb9e0796b8614d07a1346861817e92
illus0r <zor667@gmail.com>
2019-04-03 22:52:34 +0400

    no comments

--- script.js
+++ script.js
@@ -32,14 +32,39 @@
 
 
 function setup() {
-  var canvas = createCanvas(100, 100);
-  canvas.parent('canvas-wrapper');
-  background(255, 0, 200);
-	ellipse(50, 50, 80, 80);
+  var canvas = createCanvas(56, 56)
+	canvas.parent('canvas-wrapper')
+  noStroke()
+  noiseDetail(1)
+}
+
+function drawK(time, pos, color, yK, v1, v2){
+  fill(color)
+  var widthK = width*noise(time, pos, 0)*1.5
+  var crossPos = createVector(widthK, yK)
+  var armPos = crossPos.copy().add(v1)
+  var legPos = crossPos.copy().add(v2)
+  point(crossPos.x, crossPos.y)
+  rect(0, 0, widthK, height)
+  beginShape()
+  vertex(armPos.x, armPos.y)
+  vertex(crossPos.x, crossPos.y)
+  vertex(legPos.x, legPos.y)
+  endShape(CLOSE)
 }
 
 function draw() {
+  blendMode(NORMAL)
+  background('white')
+  blendMode(MULTIPLY)
+  var time = frameCount/200
+  var pos = mouseX / 100
+  var timeSplit = 0
+  var posSplit = 10
+  var yK = height*noise(time, pos, 100)*1.5
+  var v1 = p5.Vector.fromAngle(radians(45), width*2)
+  var v2 = p5.Vector.fromAngle(radians(-45), width*2)
+  drawK(time, pos, 'cyan', yK, v1, v2)
+  drawK(time+timeSplit, pos+posSplit, 'yellow', yK, v1, v2)
+  drawK(time+2*timeSplit, pos+2*posSplit, 'magenta', yK, v1, v2)
 }
-
-
-
