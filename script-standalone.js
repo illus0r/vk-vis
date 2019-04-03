64d46435489ea23f29efcf76f8bae5b5d45e9bbb
illus0r <zor667@gmail.com>
2019-04-03 16:42:48 +0400

    no comments

---
+++ script-standalone.js
@@ -0,0 +1,45 @@
+VK.init(function() { 
+	//VK.callMethod("showSettingsBox", 4096);
+	VK.callMethod("showSettingsBox", 2);
+	VK.addCallback('onSettingsChanged', function f(data){ 
+		console.log("settings: " + data); 
+		console.log('>>> Fetching…');
+		VK.api("friends.get",
+						{"user_id": "85504912",
+							"count": "10",
+							"v":"5.8"},
+						function (data) {
+							console.log('>>> friends:');
+							console.log(data);
+						});
+		//VK.api("messages.getHistory",
+						//{"peer_id": "184177213",
+							//"count": "20",
+							//"v":"5.80"},
+						//function (data) {
+							//console.log('>>> history is here:');
+							//console.log(data);
+						//});
+	});
+}, function() { 
+	// API initialization failed 
+	// Can reload page here 
+	console.log('>>> vk init failed');
+}, '5.92'); 
+
+
+// p5js
+
+
+function setup() {
+  var canvas = createCanvas(100, 100);
+  canvas.parent('canvas-wrapper');
+  background(255, 0, 200);
+	ellipse(50, 50, 80, 80);
+}
+
+function draw() {
+}
+
+
+
