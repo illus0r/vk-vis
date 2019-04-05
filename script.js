54205c1106cd2c02337bd89727ed344086c7258d
illus0r <zor667@gmail.com>
2019-04-06 01:51:52 +0400

    no comments

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 var hash = window.location.hash.substr(1);
 
 var result = hash.split('&').reduce(function (result, item) {
-    var parts = item.split('=');
-    result[parts[0]] = parts[1];
-    return result;
+  var parts = item.split('=');
+  result[parts[0]] = parts[1];
+  return result;
 }, {});
 
 if(!result.access_token){
@@ -13,37 +13,51 @@ if(!result.access_token){
 else{
   console.log(result.access_token)
   var script = document.createElement('SCRIPT');
-  script.src = "https://api.vk.com/method/friends.get?count=128&fields=nickname,photo_50&access_token="+result.access_token+"&v=5.8&callback=callbackFunc";
+  script.src = "https://api.vk.com/method/friends.get?fields=nickname,photo_50&access_token="+result.access_token+"&v=5.8&callback=callbackFunc";
   document.getElementsByTagName("head")[0].appendChild(script);
   function callbackFunc(d) {
-    console.log(d)
     d.response.items.forEach(function(friend){
-      console.log(friend.photo_50)
     })
-    // p5
-    
     var friends = d.response.items;
+    // p5
 
-    document.write('Hello World!');
+
     const sketch = (p) => {
       p.preload = () => {
         //logo = p.loadImage('https://pp.userapi.com/c846121/v846121012/137080/hS3GMvGZEkI.jpg?ava=1');
       }
       p.setup = () => {
-        var canvas = p.createCanvas(256, 256)
-        for(var i = 0; i < friends.length; i++){
-           p.loadImage(friends[i].photo_50, img =>{
+        var canvas = p.createCanvas(p.windowWidth, p.windowHeight)
+        for(let i = 0; i < friends.length; i++){
+          //console.log('>>> loading image '+ friends[i].photo_50)
+          p.loadImage(friends[i].photo_50, img =>{
+            console.log('>>> success 👾')
             friends[i].photo_50_img = img
-            p.image(img, 0, 0);
-           }, err => {
-             console.log('CORS, mofucker')
-           });
+          }, err => {
+            console.log('>>> skip 🙀')
+            console.log(friends, i)
+            // making empty image
+            var img = p.createImage(50, 50);
+            img.loadPixels();
+            for (let i = 0; i < img.width; i++) {
+              for (let j = 0; j < img.height; j++) {
+                img.set(i, j, p.color(0, 90, 102));
+              }
+            }
+            img.updatePixels();
+            friends[i].photo_50_img = img
+          });
         }
-        p.frameRate(4)
+        //p.frameRate(4)
       }
       p.draw = () => {
         let img = p.random(friends).photo_50_img
-        p.image(img, p.random(200), p.random(200))
+        if(img){
+          p.image(img, p.random(p.width-50), p.random(p.height-50))
+        }
+        else{
+          console.log('wop((')
+        }
       }
     }
     let myp5 = new p5(sketch)
